fa647d07e7f6b0dbd66702f3d15c259f1c3a747d
HP <HP@192.168.0.230>
2025-02-11 13:16:32 +0530

    day 3

---
+++ DayThree/src/com/tnsif/daythree/constructor/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.daythree.constructor;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptdemo obj=new OopsConceptdemo();
+		obj.setName("Abhi");
+		obj.setAge(30);
+		obj.setSerailnum(12345);
+		
+		System.out.println(obj);
+
+	}
+
+}
