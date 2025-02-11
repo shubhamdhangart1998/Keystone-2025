794b7080634c2c844cc47c6cc98721a03fefeae7
HP <HP@192.168.0.230>
2025-02-11 14:08:24 +0530

    Day 4

---
+++ DayFour/src/com/tnsif/dayfour/firstpackage/Base.java
@@ -0,0 +1,45 @@
+package com.tnsif.dayfour.firstpackage;
+
+public class Base {
+
+	
+	//declare all access modifier varibles
+	
+	int varDefault=10;
+	public int varPublic=20;
+	private int varPrivate=30;
+	protected int varProtected=40;
+	
+	//declare a methods with diff access modifier
+	
+	void methodDeafult()
+	{
+	
+		System.out.println("default access Base class ");
+		System.out.println("default varible: "+ varDefault);
+	}
+	
+	public void methodPublic()
+	{
+	
+		System.out.println("Public access Base class ");
+		System.out.println("Public varible: "+ varPublic);
+	}
+	
+	private void methodPrivate()
+	{
+	
+		System.out.println("Private access Base class ");
+		System.out.println("Private varible: "+ varPrivate);
+	}
+	
+	protected void methodProtected()
+	{
+	
+		System.out.println("Protected access Base class ");
+		System.out.println("Protected varible: "+ varProtected);
+	}
+	
+	
+	
+}
